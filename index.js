4e81ae4307ea83a0eaf9160cec5f66c147493f16
unknown <mrehawk82@gmail.com>
2020-02-19 10:33:26 -0500

    ...

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var data = ["-","gate gaurdian","the living room","Biological Motherboard"];
+var data = ["-","Gate Gaurdian","The \"Living\" Room","Biological Motherboard"];
 
 exports.lookup = function (x) {
 	return data[x];
